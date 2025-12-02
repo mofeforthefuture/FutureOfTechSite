6a9dbdaa8094ee3a0be11661ff1bc4f38cefda80
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-12-02 21:06:27 +0100

    chore: add heroui provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { HeroUIProvider } from "@heroui/system";
+
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HeroUIProvider>
+    <App />
+  </HeroUIProvider>
+);
